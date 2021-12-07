2a5be4af9aef97cce47e1ced090a220384a63450
nickf2k <38048839+tr0jan666@users.noreply.github.com>
2021-12-07 22:51:54 +0700

    init task

--- src/main/java/com/example/quanlyphuong/HelloApplication.java
+++ src/main/java/com/example/quanlyphuong/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Quan ly phuong - branch 2021Dec07_loginUI");
         stage.setScene(scene);
         stage.show();
     }
