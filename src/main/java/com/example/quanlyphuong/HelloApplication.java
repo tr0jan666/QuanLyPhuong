2eba7fd8f7dc95f22f7439c1f2d6a0c42a923686
nickf2k <38048839+tr0jan666@users.noreply.github.com>
2021-12-07 23:01:09 +0700

    validate username done!!!

--- src/main/java/com/example/quanlyphuong/HelloApplication.java
+++ src/main/java/com/example/quanlyphuong/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Quan ly phuong - branch 2021Dec07_loginUI");
+        stage.setTitle("Quan ly phuong - branch 2021Dec07_loginUI version 2");
         stage.setScene(scene);
         stage.show();
     }
