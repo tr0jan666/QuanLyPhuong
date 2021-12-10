a896cfec0643f4a7e3fe79795f1971f6fab606c2
nickf2k <38048839+tr0jan666@users.noreply.github.com>
2021-12-10 10:45:57 +0700

    add constant value

---
+++ src/main/java/com/example/quanlyphuong/helper/constant/GioiTinhConstant.java
@@ -0,0 +1,6 @@
+package com.example.quanlyphuong.helper.constant;
+
+public class GioiTinhConstant {
+    public static final int NAM = 1;
+    public static final int NU = 0;
+}
---
+++ src/main/java/com/example/quanlyphuong/helper/constant/KetQuaTestConstant.java
@@ -0,0 +1,6 @@
+package com.example.quanlyphuong.helper.constant;
+
+public class KetQuaTestConstant {
+    public static final int DUONG_TINH = 1;
+    public static final int AM_TINH = 0;
+}
