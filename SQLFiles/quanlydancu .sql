1c0af087cb45ac9c60414ec086e353aeedef47a2
phannguyen020501 <phannguyen147852@gmail.com>
2021-12-10 11:23:46 +0700

    phannguyen commit sql file/ chỉnh sửa bổ sung

--- SQLFiles/quanlydancu .sql
+++ SQLFiles/quanlydancu .sql
@@ -23,15 +23,15 @@ DROP TABLE IF EXISTS `cach_ly`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cach_ly` (
-  `id_CachLy` int NOT NULL,
-  `id_nhankhau` int NOT NULL,
-  `diaDiemCachLy` varchar(100) NOT NULL,
-  `thoiGianBatDau` datetime NOT NULL,
-  `muDoCachLy` varchar(50) NOT NULL,
-  `hetBenh` bit(2) NOT NULL,
-  PRIMARY KEY (`id_CachLy`),
-  KEY `id_NhanKhau_idx` (`id_nhankhau`),
-  CONSTRAINT `id_NhanKhau` FOREIGN KEY (`id_nhankhau`) REFERENCES `test` (`id_nhankhau`)
+                           `idCachLy` int NOT NULL,
+                           `idNhankhau` int NOT NULL,
+                           `diaDiemCachLy` varchar(100) NOT NULL,
+                           `thoiGianBatDau` datetime NOT NULL,
+                           `muDoCachLy` int NOT NULL,
+                           `hetBenh` bit(2) NOT NULL,
+                           PRIMARY KEY (`idCachLy`),
+                           KEY `id_NhanKhau_idx` (`idNhankhau`),
+                           CONSTRAINT `id_NhanKhau` FOREIGN KEY (`idNhankhau`) REFERENCES `test` (`idNhankhau`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -52,14 +52,14 @@ DROP TABLE IF EXISTS `khai_bao`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `khai_bao` (
-  `id_khaibao` int NOT NULL,
-  `id_nhankhau` int NOT NULL,
-  `vung_dich` varchar(50) NOT NULL,
-  `bieu_hien` varchar(50) NOT NULL,
-  `ngay_khai_bao` datetime NOT NULL,
-  PRIMARY KEY (`id_khaibao`),
-  KEY `id_nhankhau_kb_fk_idx` (`id_nhankhau`),
-  CONSTRAINT `id_nhankhau_kb_fk` FOREIGN KEY (`id_nhankhau`) REFERENCES `quan_ly_nhan_khau`.`nhan_khau` (`ID`)
+                            `idKhaiBao` int NOT NULL,
+                            `idNhanKhau` int NOT NULL,
+                            `vungDich` varchar(50) NOT NULL,
+                            `bieuHien` varchar(50) NOT NULL,
+                            `ngayKhaiBao` datetime NOT NULL,
+                            PRIMARY KEY (`idKhaiBao`),
+                            KEY `id_nhankhau_kb_fk_idx` (`idNhanKhau`),
+                            CONSTRAINT `id_nhankhau_kb_fk` FOREIGN KEY (`idNhanKhau`) REFERENCES `quan_ly_nhan_khau`.`nhan_khau` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -80,14 +80,14 @@ DROP TABLE IF EXISTS `test`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `test` (
-  `id_test` int NOT NULL,
-  `id_nhankhau` int NOT NULL,
-  `thoi_diem_test` datetime NOT NULL,
-  `hinh_thuc_test` varchar(50) NOT NULL,
-  `ket_qua` varchar(50) NOT NULL,
-  PRIMARY KEY (`id_test`),
-  KEY `id_nhankhau_fk` (`id_nhankhau`),
-  CONSTRAINT `id_nhankhau_fk` FOREIGN KEY (`id_nhankhau`) REFERENCES `quan_ly_nhan_khau`.`nhan_khau` (`ID`)
+                        `idTest` int NOT NULL,
+                        `idNhanKhau` int NOT NULL,
+                        `thoiDiemTest` datetime NOT NULL,
+                        `hinhThucTest` varchar(50) NOT NULL,
+                        `ketQua` int NOT NULL,
+                        PRIMARY KEY (`idTest`),
+                        KEY `id_nhankhau_fk` (`idNhanKhau`),
+                        CONSTRAINT `id_nhankhau_fk` FOREIGN KEY (`idNhanKhau`) REFERENCES `quan_ly_nhan_khau`.`nhan_khau` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -108,14 +108,14 @@ DROP TABLE IF EXISTS `tiem_chung`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tiem_chung` (
-  `idTiemChung` int NOT NULL,
-  `idNhanKhau` int NOT NULL,
-  `soLanTiem` int NOT NULL,
-  `ngayTiem` datetime NOT NULL,
-  `vacxin` varchar(50) NOT NULL,
-  PRIMARY KEY (`idTiemChung`),
-  KEY `id_nhankhau_fk` (`idNhanKhau`),
-  CONSTRAINT `id_nhankhau_tc_fk` FOREIGN KEY (`idNhanKhau`) REFERENCES `quan_ly_nhan_khau`.`nhan_khau` (`ID`)
+                              `idTiemChung` int NOT NULL,
+                              `idNhanKhau` int NOT NULL,
+                              `soLanTiem` int NOT NULL,
+                              `ngayTiem` datetime NOT NULL,
+                              `vacxin` varchar(50) NOT NULL,
+                              PRIMARY KEY (`idTiemChung`),
+                              KEY `id_nhankhau_fk` (`idNhanKhau`),
+                              CONSTRAINT `id_nhankhau_tc_fk` FOREIGN KEY (`idNhanKhau`) REFERENCES `quan_ly_nhan_khau`.`nhan_khau` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -163,14 +163,14 @@ DROP TABLE IF EXISTS `cmt/cccd`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cmt/cccd` (
-  `ID` int NOT NULL,
-  `idNhanKhau` int DEFAULT NULL,
-  `soCMT` varchar(12) NOT NULL,
-  `ngayCap` date DEFAULT NULL,
-  `noiCap` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `idNhankhau_fk_idx` (`idNhanKhau`),
-  CONSTRAINT `idNhankhau_fk` FOREIGN KEY (`idNhanKhau`) REFERENCES `nhan_khau` (`ID`)
+                            `ID` int NOT NULL,
+                            `idNhanKhau` int DEFAULT NULL,
+                            `soCMT` varchar(12) NOT NULL,
+                            `ngayCap` date DEFAULT NULL,
+                            `noiCap` varchar(100) DEFAULT NULL,
+                            PRIMARY KEY (`ID`),
+                            KEY `idNhankhau_fk_idx` (`idNhanKhau`),
+                            CONSTRAINT `idNhankhau_fk` FOREIGN KEY (`idNhanKhau`) REFERENCES `nhan_khau` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -191,18 +191,18 @@ DROP TABLE IF EXISTS `dinh_chinh`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `dinh_chinh` (
-  `ID` int NOT NULL,
-  `idHoKhau` int DEFAULT NULL,
-  `thongTinThayDoi` varchar(100) DEFAULT NULL,
-  `thayDoiTu` varchar(100) DEFAULT NULL,
-  `thayDoiThanh` varchar(100) DEFAULT NULL,
-  `ngayThayDoi` date DEFAULT NULL,
-  `idNguoiThayDoi` int DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `idHoKhau` (`idHoKhau`) /*!80000 INVISIBLE */,
-  KEY `idNguoiThayDoi` (`idNguoiThayDoi`),
-  CONSTRAINT `idHoKhau` FOREIGN KEY (`idHoKhau`) REFERENCES `ho_khau` (`ID`),
-  CONSTRAINT `idNguoiThayDoi` FOREIGN KEY (`idNguoiThayDoi`) REFERENCES `nhan_khau` (`ID`)
+                              `ID` int NOT NULL,
+                              `idHoKhau` int DEFAULT NULL,
+                              `thongTinThayDoi` varchar(100) DEFAULT NULL,
+                              `thayDoiTu` varchar(100) DEFAULT NULL,
+                              `thayDoiThanh` varchar(100) DEFAULT NULL,
+                              `ngayThayDoi` date DEFAULT NULL,
+                              `idNguoiThayDoi` int DEFAULT NULL,
+                              PRIMARY KEY (`ID`),
+                              KEY `idHoKhau` (`idHoKhau`) /*!80000 INVISIBLE */,
+                              KEY `idNguoiThayDoi` (`idNguoiThayDoi`),
+                              CONSTRAINT `idHoKhau` FOREIGN KEY (`idHoKhau`) REFERENCES `ho_khau` (`ID`),
+                              CONSTRAINT `idNguoiThayDoi` FOREIGN KEY (`idNguoiThayDoi`) REFERENCES `nhan_khau` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -223,18 +223,18 @@ DROP TABLE IF EXISTS `ho_khau`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ho_khau` (
-  `ID` int NOT NULL,
-  `maHoKhau` varchar(100) DEFAULT NULL,
-  `idChuHo` int DEFAULT NULL,
-  `maKhuVuc` varchar(100) DEFAULT NULL,
-  `diaChi` varchar(100) DEFAULT NULL,
-  `ngayLap` date DEFAULT NULL,
-  `ngayDiChuyen` date DEFAULT NULL,
-  `lyDoChuyen` text,
-  `nguoiThucHien` int DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `sd_idx` (`maHoKhau`),
-  CONSTRAINT `maNhanKhau` FOREIGN KEY (`maHoKhau`) REFERENCES `nhan_khau` (`maNhankhau`)
+                           `ID` int NOT NULL,
+                           `maHoKhau` varchar(100) DEFAULT NULL,
+                           `idChuHo` int DEFAULT NULL,
+                           `maKhuVuc` varchar(100) DEFAULT NULL,
+                           `diaChi` varchar(100) DEFAULT NULL,
+                           `ngayLap` date DEFAULT NULL,
+                           `ngayDiChuyen` date DEFAULT NULL,
+                           `lyDoChuyen` text,
+                           `nguoiThucHien` int DEFAULT NULL,
+                           PRIMARY KEY (`ID`),
+                           KEY `sd_idx` (`maHoKhau`),
+                           CONSTRAINT `maNhanKhau` FOREIGN KEY (`maHoKhau`) REFERENCES `nhan_khau` (`maNhankhau`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -255,18 +255,18 @@ DROP TABLE IF EXISTS `khai_tu`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `khai_tu` (
-  `ID` int NOT NULL,
-  `soGiayKhaiTu` varchar(100) DEFAULT NULL,
-  `idNguoiKhai` int DEFAULT NULL,
-  `idNguoiChet` int DEFAULT NULL,
-  `ngayKhai` date DEFAULT NULL,
-  `ngayChet` date DEFAULT NULL,
-  `lyDoChet` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `idNguoiKhai` (`idNguoiKhai`) /*!80000 INVISIBLE */,
-  KEY `idNguoiChet` (`idNguoiChet`),
-  CONSTRAINT `idNguoiChet` FOREIGN KEY (`idNguoiChet`) REFERENCES `nhan_khau` (`ID`),
-  CONSTRAINT `idNguoiKhai` FOREIGN KEY (`idNguoiKhai`) REFERENCES `nhan_khau` (`ID`)
+                           `ID` int NOT NULL,
+                           `soGiayKhaiTu` varchar(100) DEFAULT NULL,
+                           `idNguoiKhai` int DEFAULT NULL,
+                           `idNguoiChet` int DEFAULT NULL,
+                           `ngayKhai` date DEFAULT NULL,
+                           `ngayChet` date DEFAULT NULL,
+                           `lyDoChet` varchar(100) DEFAULT NULL,
+                           PRIMARY KEY (`ID`),
+                           KEY `idNguoiKhai` (`idNguoiKhai`) /*!80000 INVISIBLE */,
+                           KEY `idNguoiChet` (`idNguoiChet`),
+                           CONSTRAINT `idNguoiChet` FOREIGN KEY (`idNguoiChet`) REFERENCES `nhan_khau` (`ID`),
+                           CONSTRAINT `idNguoiKhai` FOREIGN KEY (`idNguoiKhai`) REFERENCES `nhan_khau` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -287,39 +287,39 @@ DROP TABLE IF EXISTS `nhan_khau`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `nhan_khau` (
-  `ID` int NOT NULL,
-  `maNhankhau` varchar(100) DEFAULT 'null',
-  `Ho_ten` varchar(45) DEFAULT 'null',
-  `namSinh` date DEFAULT NULL,
-  `gioiTinh` varchar(100) DEFAULT 'null',
-  `noiSinh` varchar(100) DEFAULT 'null',
-  `nguyenQuan` varchar(100) DEFAULT 'null',
-  `danToc` varchar(100) DEFAULT 'null',
-  `tonGiao` varchar(100) DEFAULT 'null',
-  `quocTich` varchar(100) DEFAULT 'null',
-  `noiThuongTru` varchar(100) DEFAULT 'null',
-  `diaChiHienNay` varchar(100) DEFAULT 'null',
-  `tinhDoHocVan` varchar(100) DEFAULT 'null',
-  `ngheNghiep` varchar(100) DEFAULT 'null',
-  `noiLamViec` varchar(100) DEFAULT 'null',
-  `tienAn` varchar(100) DEFAULT 'null',
-  `ngayChuyenDen` date DEFAULT NULL,
-  `lyDoChuyenDen` varchar(100) DEFAULT 'null',
-  `ngayChuyenDi` date DEFAULT NULL,
-  `lyDoChuyenDi` varchar(100) DEFAULT 'null',
-  `diaChiMoi` varchar(100) DEFAULT 'null',
-  `ngayTao` date DEFAULT NULL,
-  `idNguoiTao` int DEFAULT NULL,
-  `ngayXoa` date DEFAULT NULL,
-  `idNguoiXoa` int DEFAULT NULL,
-  `lyDoXoa` varchar(100) DEFAULT 'null',
-  `ghiChu` varchar(100) DEFAULT 'null',
-  PRIMARY KEY (`ID`),
-  KEY `maNhanKhau` (`maNhankhau`) /*!80000 INVISIBLE */,
-  KEY `idNguoiTao_idx` (`idNguoiTao`),
-  KEY `idNguoiXoa_idx` (`idNguoiXoa`),
-  CONSTRAINT `idNguoiTao` FOREIGN KEY (`idNguoiTao`) REFERENCES `user` (`ID`),
-  CONSTRAINT `idNguoiXoa` FOREIGN KEY (`idNguoiXoa`) REFERENCES `user` (`ID`)
+                             `ID` int NOT NULL,
+                             `maNhanKhau` varchar(100) DEFAULT 'null',
+                             `hoTen` varchar(45) DEFAULT 'null',
+                             `namSinh` date DEFAULT NULL,
+                             `gioiTinh` int DEFAULT NULL,
+                             `noiSinh` varchar(100) DEFAULT 'null',
+                             `nguyenQuan` varchar(100) DEFAULT 'null',
+                             `danToc` varchar(100) DEFAULT 'null',
+                             `tonGiao` varchar(100) DEFAULT 'null',
+                             `quocTich` varchar(100) DEFAULT 'null',
+                             `noiThuongTru` varchar(100) DEFAULT 'null',
+                             `diaChiHienNay` varchar(100) DEFAULT 'null',
+                             `tinhDoHocVan` varchar(100) DEFAULT 'null',
+                             `ngheNghiep` varchar(100) DEFAULT 'null',
+                             `noiLamViec` varchar(100) DEFAULT 'null',
+                             `tienAn` varchar(100) DEFAULT 'null',
+                             `ngayChuyenDen` date DEFAULT NULL,
+                             `lyDoChuyenDen` varchar(100) DEFAULT 'null',
+                             `ngayChuyenDi` date DEFAULT NULL,
+                             `lyDoChuyenDi` varchar(100) DEFAULT 'null',
+                             `diaChiMoi` varchar(100) DEFAULT 'null',
+                             `ngayTao` date DEFAULT NULL,
+                             `idNguoiTao` int DEFAULT NULL,
+                             `ngayXoa` date DEFAULT NULL,
+                             `idNguoiXoa` int DEFAULT NULL,
+                             `lyDoXoa` varchar(100) DEFAULT 'null',
+                             `ghiChu` varchar(100) DEFAULT 'null',
+                             PRIMARY KEY (`ID`),
+                             KEY `maNhanKhau` (`maNhankhau`) /*!80000 INVISIBLE */,
+                             KEY `idNguoiTao_idx` (`idNguoiTao`),
+                             KEY `idNguoiXoa_idx` (`idNguoiXoa`),
+                             CONSTRAINT `idNguoiTao` FOREIGN KEY (`idNguoiTao`) REFERENCES `user` (`ID`),
+                             CONSTRAINT `idNguoiXoa` FOREIGN KEY (`idNguoiXoa`) REFERENCES `user` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -340,15 +340,15 @@ DROP TABLE IF EXISTS `tam_tru`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tam_tru` (
-  `ID` int NOT NULL,
-  `idNhanKhau` int DEFAULT NULL,
-  `maGiayTamTru` varchar(100) DEFAULT NULL,
-  `tuNgay` date DEFAULT NULL,
-  `denNgay` date DEFAULT NULL,
-  `lyDo` text,
-  PRIMARY KEY (`ID`),
-  KEY `idNhanKhau_fk` (`idNhanKhau`),
-  CONSTRAINT `idNhanKhau_tt_fk` FOREIGN KEY (`idNhanKhau`) REFERENCES `nhan_khau` (`ID`)
+                           `ID` int NOT NULL,
+                           `idNhanKhau` int DEFAULT NULL,
+                           `maGiayTamTru` varchar(100) DEFAULT NULL,
+                           `tuNgay` date DEFAULT NULL,
+                           `denNgay` date DEFAULT NULL,
+                           `lyDo` text,
+                           PRIMARY KEY (`ID`),
+                           KEY `idNhanKhau_fk` (`idNhanKhau`),
+                           CONSTRAINT `idNhanKhau_tt_fk` FOREIGN KEY (`idNhanKhau`) REFERENCES `nhan_khau` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -369,15 +369,15 @@ DROP TABLE IF EXISTS `tam_vang`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tam_vang` (
-  `ID` int NOT NULL,
-  `idNhanKhau` int DEFAULT NULL,
-  `maGiayTamVang` varchar(100) DEFAULT NULL,
-  `tuNgay` date DEFAULT NULL,
-  `denNgay` date DEFAULT NULL,
-  `lyDo` text,
-  PRIMARY KEY (`ID`),
-  KEY `idNhanKhau` (`idNhanKhau`),
-  CONSTRAINT `idNhanKhau_tv_fk` FOREIGN KEY (`idNhanKhau`) REFERENCES `nhan_khau` (`ID`)
+                            `ID` int NOT NULL,
+                            `idNhanKhau` int DEFAULT NULL,
+                            `maGiayTamVang` varchar(100) DEFAULT NULL,
+                            `tuNgay` date DEFAULT NULL,
+                            `denNgay` date DEFAULT NULL,
+                            `lyDo` text,
+                            PRIMARY KEY (`ID`),
+                            KEY `idNhanKhau` (`idNhanKhau`),
+                            CONSTRAINT `idNhanKhau_tv_fk` FOREIGN KEY (`idNhanKhau`) REFERENCES `nhan_khau` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -398,13 +398,13 @@ DROP TABLE IF EXISTS `thanh_vien_cua_ho`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `thanh_vien_cua_ho` (
-  `idNhanKhau` int NOT NULL,
-  `idHoKhau` int NOT NULL,
-  `quanHeVoiChuHo` varchar(100) DEFAULT NULL,
-  KEY `idHoKhau_fk_idx` (`idHoKhau`),
-  KEY `id_NhanKhau_thch_fk_idx` (`idNhanKhau`),
-  CONSTRAINT `id_NhanKhau_thch_fk` FOREIGN KEY (`idNhanKhau`) REFERENCES `nhan_khau` (`ID`),
-  CONSTRAINT `idHoKhau_fk` FOREIGN KEY (`idHoKhau`) REFERENCES `ho_khau` (`ID`)
+                                     `idNhanKhau` int NOT NULL,
+                                     `idHoKhau` int NOT NULL,
+                                     `quanHeVoiChuHo` varchar(100) DEFAULT NULL,
+                                     KEY `idHoKhau_fk_idx` (`idHoKhau`),
+                                     KEY `id_NhanKhau_thch_fk_idx` (`idNhanKhau`),
+                                     CONSTRAINT `id_NhanKhau_thch_fk` FOREIGN KEY (`idNhanKhau`) REFERENCES `nhan_khau` (`ID`),
+                                     CONSTRAINT `idHoKhau_fk` FOREIGN KEY (`idHoKhau`) REFERENCES `ho_khau` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -425,10 +425,10 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `ID` int NOT NULL,
-  `userName` varchar(50) NOT NULL,
-  `Password` varchar(50) NOT NULL,
-  PRIMARY KEY (`ID`)
+                        `ID` int NOT NULL,
+                        `userName` varchar(50) NOT NULL,
+                        `Password` varchar(50) NOT NULL,
+                        PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
