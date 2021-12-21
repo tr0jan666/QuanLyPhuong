01bd3a0bff83fcacab3c2b4df9c24ebdc71d4146
DINH CHI CONG <dinhchicongf9@gmail.com>
2021-12-21 23:52:05 +0700

    update Tao, Sua

--- src/main/java/com/example/quanlyphuong/services/NhanKhauService.java
+++ src/main/java/com/example/quanlyphuong/services/NhanKhauService.java
@@ -58,13 +58,44 @@ public class NhanKhauService {
         int idNguoiXoa= nhanKhauMoi.getIdNguoiXoa();
         String lyDoXoa= nhanKhauMoi.getLyDoXoa();
         String ghiChu= nhanKhauMoi.getGhiChu();
-        TiemChungModel thongTinTiemChung= nhanKhauMoi.getThongTinTiemChung();
-        CachLyModel thongTinCachLy= nhanKhauMoi.getThongTinCachLy();
 
         try{
+            // ket noi voi data_base
             Connection connection = MySQLConnector.getConnection();
-            String query = " ";
+            String query = "INSERT INTO nhan_khau VALUE (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            // cai dat gia tri
+            preparedStatement.setInt(1, ID);
+            preparedStatement.setInt(2, maNhanKhau);
+            preparedStatement.setString(3, Ho_ten);
+            preparedStatement.setDate(4, namSinh);
+            preparedStatement.setString(5, gioiTinh);
+            preparedStatement.setString(6, noiSinh );
+            preparedStatement.setString(7, nguyenQuan);
+            preparedStatement.setString(8, danToc );
+            preparedStatement.setString(9, tonGiao);
+            preparedStatement.setString(10, quocTich );
+            preparedStatement.setString(11, noiThuongTru);
+            preparedStatement.setString(12, soHoChieu );
+            preparedStatement.setString(13, diaChiHienNay );
+            preparedStatement.setString(14, trinhDoHocVan);
+            preparedStatement.setString(15, ngheNghiep );
+            preparedStatement.setString(16, noiLamViec);
+            preparedStatement.setString(17, tienAn );
+            preparedStatement.setDate(18, ngayChuyenDen);
+            preparedStatement.setString(19, lyDoChuyenDen );
+            preparedStatement.setDate(20, ngayChuyenDi);
+            preparedStatement.setString(21, lyDoChuyenDi);
+            preparedStatement.setString(22, diaChiMoi );
+            preparedStatement.setDate(23, ngayTao);
+            preparedStatement.setInt(24, idNguoiTao);
+            preparedStatement.setDate(25, ngayXoa);
+            preparedStatement.setInt(26, idNguoiXoa);
+            preparedStatement.setString(27, lyDoXoa);
+            preparedStatement.setString(28, ghiChu);
+
+            // dong co so du lieu
             preparedStatement.executeUpdate();
             preparedStatement.close();
         } catch (SQLException ex) {// thong bao loi
@@ -73,10 +104,70 @@ public class NhanKhauService {
         }
     }
 
-    public SimpleResult suaNhanKhau(int idNhanKhau) {
+    public SimpleResult suaNhanKhau(NhanKhauModel nhanKhauMoi) {
+
+        // lay thong tin input
+        int ID = nhanKhauMoi.getID();
+        String maNhanKhau = nhanKhauMoi.getMaNhanKhau();
+        String Ho_ten = nhanKhauMoi.getHo_ten();
+        Date namSinh = nhanKhauMoi.getNamSinh();
+        String gioiTinh = nhanKhauMoi.getGioiTinh();
+        String noiSinh = nhanKhauMoi.getNoiSinh();
+        String nguyenQuan = nhanKhauMoi.getNguyenQuan();
+        String danToc = nhanKhauMoi.getDanToc();
+        String tonGiao = nhanKhauMoi.getTonGiao();
+        String quocTich= nhanKhauMoi.getQuocTich();
+        String noiThuongTru= nhanKhauMoi.getNoiThuongTru();
+        String soHoChieu= nhanKhauMoi.getSoHoChieu();
+        String diaChiHienNay= nhanKhauMoi.getDiaChiHienNay();
+        String trinhDoHocVan= nhanKhauMoi.getTrinhDoHocVan();
+        String ngheNghiep= nhanKhauMoi.getNgheNghiep();
+        String noiLamViec= nhanKhauMoi.getNoiLamViec();
+        String tienAn= nhanKhauMoi.getTienAn();
+        Date ngayChuyenDen= nhanKhauMoi.getNgayChuyenDen();
+        String lyDoChuyenDen= nhanKhauMoi.getLyDoChuyenDen();
+        Date ngayChuyenDi= nhanKhauMoi.getNgayChuyenDi();
+        String lyDoChuyenDi= nhanKhauMoi.getLyDoChuyenDi();
+        String diaChiMoi= nhanKhauMoi.getDiaChiMoi();
+        Date ngayTao= nhanKhauMoi.getNgayTao();
+        int idNguoiTao= nhanKhauMoi.getIdNguoiTao();
+        Date ngayXoa= nhanKhauMoi.getNgayXoa();
+        int idNguoiXoa= nhanKhauMoi.getIdNguoiXoa();
+        String lyDoXoa= nhanKhauMoi.getLyDoXoa();
+        String ghiChu= nhanKhauMoi.getGhiChu();
+
         try{
             Connection connection = MySQLConnector.getConnection();
-            String query = " ";
+            String query = "UPDATE nhan_khau SET "
+                    + "maNhanKhau = " + maNhanKhau
+                    + "Ho_ten = " + Ho_ten
+                    + "namSinh = " + namSinh
+                    + "gioiTinh = " + gioiTinh
+                    + "noiSinh = " + noiSinh
+                    + "nguyenQuan = " + nguyenQuan
+                    + "danToc = " + danToc
+                    + "tonGiao = " + tonGiao
+                    + "quocTich = " + quocTich
+                    + "noiThuongTru = " + noiThuongTru
+                    + "soHoChieu = " + soHoChieu
+                    + "diaChiHienNay = " + diaChiHienNay
+                    + "trinhDoHocVan = " + trinhDoHocVan
+                    + "ngheNghiep = " + ngheNghiep
+                    + "noiLamViec = " + noiLamViec
+                    + "tienAn = " + tienAn
+                    + "ngayChuyenDen = " + ngayChuyenDen
+                    + "lyDoChuyenDen = " + lyDoChuyenDen
+                    + "ngayChuyenDi = " + ngayChuyenDi
+                    + "lyDoChuyenDi = " + lyDoChuyenDi
+                    + "diaChiMoi = " + diaChiMoi
+                    + "ngayTao = " + ngayTao
+                    + "idNguoiTao = " + idNguoiTao
+                    + "ngayXoa = " + ngayXoa
+                    + "idNguoiXoa = " + idNguoiXoa
+                    + "lyDoXoa = " + lyDoXoa
+                    + "ghiChu = " + ghiChu
+                    + "WHERE ID =" + ID;
+
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             preparedStatement.executeUpdate();
             preparedStatement.close();
