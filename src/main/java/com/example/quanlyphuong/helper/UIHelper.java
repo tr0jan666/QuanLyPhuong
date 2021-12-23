5ea13e32f9e94d811076d8c6ff2219782a397b27
huy24112001 <huyassassin24@gmail.com>
2021-12-23 11:21:57 +0700

    commit ghep code

--- src/main/java/com/example/quanlyphuong/helper/UIHelper.java
+++ src/main/java/com/example/quanlyphuong/helper/UIHelper.java
@@ -12,11 +12,19 @@ public class UIHelper {
     public static double DEFAULT_SCREEN_WIDTH = 1200;
     public static double DEFAULT_SCREEN_HEIGHT = 800;
 
+    public static AppScreen navigateNew(String fxmlPath, String title) {
+        return navigateNew(fxmlPath, title, null);
+    }
+
     public static AppScreen navigateNew(String fxmlPath, String title, Object userData) {
+        return navigateNew(fxmlPath, title, userData, DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT);
+    }
+
+    public static AppScreen navigateNew(String fxmlPath, String title, Object userData, double width, double height) {
         FXMLLoader fxmlLoader = new FXMLLoader(QuanLyNhanKhauApplication.class.getResource(fxmlPath));
         System.out.println((QuanLyNhanKhauApplication.class.getResource(fxmlPath)));
         try {
-            Scene scene = new Scene(fxmlLoader.load(), DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT);
+            Scene scene = new Scene(fxmlLoader.load(), width, height);
             Stage stage = new Stage();
             stage.setTitle(title);
             stage.setResizable(false);
@@ -26,7 +34,7 @@ public class UIHelper {
             return new AppScreen(fxmlPath, scene, fxmlLoader.getController());
         } catch (IOException e) {
             e.printStackTrace();
-            return  null;
+            return null;
         }
 
     }
