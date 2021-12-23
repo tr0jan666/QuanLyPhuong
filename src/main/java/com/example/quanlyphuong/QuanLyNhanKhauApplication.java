dfdef3850ef0834f61fa517166bd1d3857d1bccb
huy24112001 <huyassassin24@gmail.com>
2021-12-23 11:27:02 +0700

    huy ghep code btn quay lai thong ke

--- src/main/java/com/example/quanlyphuong/QuanLyNhanKhauApplication.java
+++ src/main/java/com/example/quanlyphuong/QuanLyNhanKhauApplication.java
@@ -12,7 +12,7 @@ public class QuanLyNhanKhauApplication extends Application {
     public void start(Stage stage) throws IOException {
       FXMLLoader fxmlLoader = new FXMLLoader(QuanLyNhanKhauApplication.class.getResource("login-view.fxml"));
 
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 630);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Quản lý dân cư");
         stage.setResizable(false);
         stage.setScene(scene);
