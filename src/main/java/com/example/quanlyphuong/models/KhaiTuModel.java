4fc6b19b7e1aab9c916ac323eef11f68974c364e
quanngotrong <ngotrongquan116@gmail.com>
2021-12-24 05:56:01 +0700

    xong khai tu

--- src/main/java/com/example/quanlyphuong/models/KhaiTuModel.java
+++ src/main/java/com/example/quanlyphuong/models/KhaiTuModel.java
@@ -1,87 +1,79 @@
 package com.example.quanlyphuong.models;
-    public class KhaiTuModel {
+
+import java.util.Date;
+
+public class KhaiTuModel {
         private int ID;
-        private String soCMTnguoiKhai;
-        private String soCMTnguoiMat;
-        private String tenNguoiKhai;
         private String soGiayKhaiTu;
-        private String ngayKhai;
-        private String ngayMat;
+        private int idNguoiKhai;
+        private int idNguoiChet;
+        private Date ngayKhai;
+        private Date ngayChet;
         private String lyDoChet;
 
-        public KhaiTuModel(int ID,String soCMTnguoiKhai,String tenNguoiKhai,String soCMTnguoiMat,
-                           String soGiayKhaiTu,String ngayMat,String ngayKhai,String lyDoChet){
-            this.ID = ID;
-            this.soCMTnguoiKhai = soCMTnguoiKhai;
-            this.soCMTnguoiMat = soCMTnguoiMat;
-            this.tenNguoiKhai = tenNguoiKhai;
-            this.soGiayKhaiTu = soGiayKhaiTu;
-            this.ngayKhai = ngayKhai;
-            this.ngayMat = ngayMat;
-            this.lyDoChet = lyDoChet;
-        }
-
-        public String getSoCMTnguoiKhai() {
-            return soCMTnguoiKhai;
-        }
-
-        public void setSoCMTnguoiKhai(String soCMTnguoiKhai) {
-            this.soCMTnguoiKhai = soCMTnguoiKhai;
-        }
-
-        public String getSoCMTnguoiMat() {
-            return soCMTnguoiMat;
-        }
-
-        public void setSoCMTnguoiMat(String soCMTnguoiMat) {
-            this.soCMTnguoiMat = soCMTnguoiMat;
-        }
-
-        public String getTenNguoiKhai() {
-            return tenNguoiKhai;
-        }
-
-        public void setTenNguoiKhai(String tenNguoiKhai) {
-            this.tenNguoiKhai = tenNguoiKhai;
-        }
-
-        public String getSoGiayKhaiTu() {
-            return soGiayKhaiTu;
-        }
-
-        public void setSoGiayKhaiTu(String soGiayKhaiTu) {
-            this.soGiayKhaiTu = soGiayKhaiTu;
-        }
-
-        public String getNgayKhai() {
-            return ngayKhai;
-        }
-
-        public void setNgayKhai(String ngayKhai) {
-            this.ngayKhai = ngayKhai;
-        }
-
-        public String getNgayMat() {
-            return ngayMat;
-        }
-
-        public void setNgayMat(String ngayMat) {
-            this.ngayMat = ngayMat;
-        }
-
-        public String getLyDoChet() {
-            return lyDoChet;
-        }
-
-        public void setLyDoChet(String lyDoChet) {
-            this.lyDoChet = lyDoChet;
-        }
-
-        public int getID() {
-            return ID;
-        }
-
-        public void setID(int ID) {
-            this.ID = ID;
-        }
+    public KhaiTuModel(int ID, String soGiayKhaiTu, int idNguoiKhai, int idNguoiChet, Date ngayKhai, Date ngayChet, String lyDoChet) {
+        this.ID = ID;
+        this.soGiayKhaiTu = soGiayKhaiTu;
+        this.idNguoiKhai = idNguoiKhai;
+        this.idNguoiChet = idNguoiChet;
+        this.ngayKhai = ngayKhai;
+        this.ngayChet = ngayChet;
+        this.lyDoChet = lyDoChet;
     }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getSoGiayKhaiTu() {
+        return soGiayKhaiTu;
+    }
+
+    public void setSoGiayKhaiTu(String soGiayKhaiTu) {
+        this.soGiayKhaiTu = soGiayKhaiTu;
+    }
+
+    public int getIdNguoiKhai() {
+        return idNguoiKhai;
+    }
+
+    public void setIdNguoiKhai(int idNguoiKhai) {
+        this.idNguoiKhai = idNguoiKhai;
+    }
+
+    public int getIdNguoiChet() {
+        return idNguoiChet;
+    }
+
+    public void setIdNguoiChet(int idNguoiChet) {
+        this.idNguoiChet = idNguoiChet;
+    }
+
+    public Date getNgayKhai() {
+        return ngayKhai;
+    }
+
+    public void setNgayKhai(Date ngayKhai) {
+        this.ngayKhai = ngayKhai;
+    }
+
+    public Date getNgayChet() {
+        return ngayChet;
+    }
+
+    public void setNgayChet(Date ngayChet) {
+        this.ngayChet = ngayChet;
+    }
+
+    public String getLyDoChet() {
+        return lyDoChet;
+    }
+
+    public void setLyDoChet(String lyDoChet) {
+        this.lyDoChet = lyDoChet;
+    }
+}
