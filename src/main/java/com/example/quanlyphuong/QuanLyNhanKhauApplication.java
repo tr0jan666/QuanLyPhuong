2a229d1e250193c8ad265654b4adaf8c1086cf2d
nickf2k <38048839+tr0jan666@users.noreply.github.com>
2022-01-08 16:59:59 +0700

    go login first

--- src/main/java/com/example/quanlyphuong/QuanLyNhanKhauApplication.java
+++ src/main/java/com/example/quanlyphuong/QuanLyNhanKhauApplication.java
@@ -12,7 +12,7 @@ public class QuanLyNhanKhauApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ho_khau/Main_ho_khau.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), ScreenSizeConstant.LOGIN_WIDTH,ScreenSizeConstant.LOGIN_HEIGHT);
         stage.setTitle("Quản lý phường");
         //stage.setResizable(false);
